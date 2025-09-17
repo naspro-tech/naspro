c51ea03b40dd9778dc4b2a6ebde1a8556def7e6e
naspro-tech <shoaibmemon78609@gmail.com>
2025-09-17 10:18:40 +0500

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -68,7 +68,7 @@ export default async function handler(req, res) {
 
   try {
     // Send POST request to JazzCash REST API
-    const response = await fetch('https://payments.jazzcash.com.pk/ApplicationAPI/API/2.0/Purchase/DoMWalletTransaction', {
+    const response = await fetch('https://sandbox.jazzcash.com.pk/ApplicationAPI/API/2.0/Purchase/DoMWalletTransaction', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)

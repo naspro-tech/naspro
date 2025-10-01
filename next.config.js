e2aa1eadb5f674bb602eb28b3b0ef0694045d026
naspro-tech <shoaibmemon78609@gmail.com>
2025-10-01 13:12:15 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://naspropvt.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
+module.exports = nextConfig;

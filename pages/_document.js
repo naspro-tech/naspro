c5dab4225ab1b64a3dbc1e4c75bce6243cdf071b
naspro-tech <shoaibmemon78609@gmail.com>
2025-10-01 13:36:39 +0500

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="ocZ4mt2LvoDiiVPgHc1_y4gvd8uJ5Z9s3mMaSqQe9Vs"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

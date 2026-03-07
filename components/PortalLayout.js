8bcd9046522fd004d578618a6f3483d8348ac6d9
naspro-tech <shoaibmemon78609@gmail.com>
2026-03-08 04:20:51 +0500

    Update PortalLayout.js

--- components/PortalLayout.js
+++ components/PortalLayout.js
@@ -16,63 +16,63 @@ export default function PortalLayout({ children }) {
           Merchant Panel
         </h2>
 
-        <ul style={{ listStyle: "none", padding: 0, lineHeight: "4.2" }}>
+        <ul style={{ listStyle: "none", padding: 0, lineHeight: "6.2" }}>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal" style={{ color: "#fff", textDecoration: "none" }}>
               Dashboard
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/wallet" style={{ color: "#fff", textDecoration: "none" }}>
               Wallet
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/usdt-request" style={{ color: "#fff", textDecoration: "none" }}>
               USDT Request
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/transactions" style={{ color: "#fff", textDecoration: "none" }}>
               Transactions
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <a href="/portal/withdraw" style={{ color: "#fff", textDecoration: "none" }}>
               Withdraw
             </a>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/reports" style={{ color: "#fff", textDecoration: "none" }}>
               Report
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/add-user" style={{ color: "#fff", textDecoration: "none" }}>
               Add User
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/status-inquiry" style={{ color: "#fff", textDecoration: "none" }}>
               Status Inquiry
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/links" style={{ color: "#fff", textDecoration: "none" }}>
               Links
             </Link>
           </li>
 
-          <li style={{ marginBottom: "6px" }}>
+          <li style={{ marginBottom: "9px" }}>
             <Link href="/portal/settings" style={{ color: "#fff", textDecoration: "none" }}>
               Settings
             </Link>
